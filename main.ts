f3367231cb3f9ecfa3e132b6514b2b6641bd7e30
phmagalhaes <pedrohenrique.msantos.2001@gmail.com>
2023-11-07 17:22:16 -0300

    upload 07/11

--- main.ts
+++ main.ts
@@ -22,36 +22,115 @@ const planta: number[][] = [
     [22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22]
 ]
 
+const planta2: number[][] = [
+    [22, 22, 22, 22, 22, 33, 11, 22, 22, 22, 22, 22, 22, 11, 11, 22, 22, 22, 22, 22],
+    [22, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 22],
+    [22, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 22],
+    [22, 11, 88, 22, 22, 22, 22, 11, 11, 11, 11, 22, 22, 11, 11, 22, 22, 88, 11, 22],
+    [22, 11, 11, 22, 22, 22, 22, 11, 11, 88, 11, 22, 22, 11, 11, 22, 22, 11, 11, 22],
+    [22, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 22, 22, 11, 11, 22, 22, 88, 11, 22],
+    [22, 11, 11, 11, 11, 88, 11, 11, 11, 11, 11, 22, 22, 11, 11, 22, 22, 11, 11, 22],
+    [22, 11, 11, 22, 22, 22, 22, 11, 11, 11, 11, 22, 22, 11, 11, 22, 22, 11, 11, 22],
+    [22, 11, 11, 22, 22, 22, 22, 11, 11, 11, 11, 22, 22, 11, 11, 22, 22, 11, 11, 22],
+    [22, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 22, 22, 11, 11, 22, 22, 11, 11, 22],
+    [22, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 22, 22, 11, 11, 22, 22, 11, 11, 22],
+    [22, 11, 11, 22, 22, 22, 22, 11, 11, 11, 11, 22, 22, 11, 11, 22, 22, 11, 11, 22],
+    [22, 11, 11, 22, 22, 22, 22, 11, 11, 11, 11, 22, 22, 11, 11, 22, 22, 11, 11, 22],
+    [22, 11, 11, 88, 11, 11, 11, 11, 11, 11, 11, 22, 22, 11, 11, 22, 22, 11, 11, 22],
+    [22, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 22],
+    [22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22],
+
+]
+
 const colorir = (text: number): string => {
     if (text == 22) {
-        return color.red.bgRed(text)
+        return color.red.bgRed('  ')
     } else if (text == 11) {
-        return color.blue.bgBlue(text)
+        return color.blue.bgBlue('  ')
     } else if (text == 33) {
-        return color.green.bgGreen(text)
+        return color.green.bgGreen('  ')
     } else {
-        return color.yellow.bgYellow(text)
+        return color.yellow.bgYellow('  ')
     }
 }
 
 var x: number = 0;
 var y: number = 5;
+var passos: number = 0;
 
 const mostrarPlanta = (): void => {
     console.clear();
-    for (let i of planta) {
+    for (let i of planta2) {
         let linha: any[] = [];
         for (let e of i) {
             linha.push(colorir(e))
         }
         console.log(linha.toString().replaceAll(',', ''))
     }
+    console.log('Passos: ', passos)
 }
 
-while (true){
+const descer = (): void => {
+    if (x + 1 < planta.length && planta[x+1][y] !== 22) {
+        planta[x][y] = 11;
+        x++
+        planta[x][y] = 33
+        passos++
+    }
+}
+
+const subir = (): void => {
+    if (x - 1 >= 0 && planta[x - 1][y] !== 22) {
+        planta[x][y] = 11;
+        x = x - 1;
+        planta[x][y] = 33
+        passos++
+    }
+
+}
+
+const esquerda = (): void => {
+    if (y - 1 >= 0 && planta[x][y - 1] !== 22) {
+        planta[x][y] = 11;
+        y = y - 1;
+        planta[x][y] = 33
+        passos++;
+    }
+
+}
+
+const direita = (): void => {
+    if (y + 1 < planta[0].length && planta[x][y+1] !== 22) {
+        planta[x][y] = 11;
+        y = y + 1;
+        planta[x][y] = 33
+        passos++;
+    }
+}
+
+const verificaProduto = (): boolean => {
+    for (var linha of planta) {
+        for (var item of linha) {
+            if (item == 88) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+while (true) {
     mostrarPlanta();
-    let escolha = input('Digite uma direção (w, a, s, d) ou (q) para (q) para sair: ');
-    if (escolha.toUpperCase() == 'Q'){
+    let escolha = input('Digite uma direção (w, a, s, d) ou (q) para sair: ');
+    if (escolha.toUpperCase() == 'Q' || verificaProduto()) {
         break
-    }   
+    } else if (escolha.toUpperCase() == 'W') {
+        subir();
+    } else if (escolha.toUpperCase() == 'A') {
+        esquerda();
+    } else if (escolha.toUpperCase() == 'S') {
+        descer();
+    } else if (escolha.toUpperCase() == 'D') {
+        direita()
+    }
 }
